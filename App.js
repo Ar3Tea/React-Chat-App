f021493990a4d2568ff443ae0cd8e79ba7a67529
Ar3Tea <tbusinessr@gmail.com>
2018-06-26 13:33:56 -0400

    adding function to foundation

--- App.js
+++ App.js
@@ -5,18 +5,72 @@ import SendMessageForm from './components/SendMessageForm'
 import RoomList from './components/RoomList'
 import NewRoomForm from './components/NewRoomForm'
 
+import { tokenURL, instanceLocator } from './config'
+
 class App extends React.Component {
 
-    componentDidMount() {
+    constructor() {
+        super()
+        this.state = {
+            messages: [],
+            joinableRooms: [],
+            joinedRooms: []
+        }
 
+        this.sendMessage = this.sendMessage.bind(this);
+    }
+
+    componentDidMount() {
+      const chatManager = new Chatkit.ChatManager({
+          instanceLocator,
+          userID: 'Corey',
+          tokenProvider: new Chatkit.TokenProvider({
+              url: tokenUrl
+          })
+      })
+
+      chatManager.connect()
+      .then(currentUser => {
+          this.currentUser = currentUser
+
+          this.currentUsergetJoinableRooms()
+          .then(joinableRooms => {
+              this.setState({
+                  joinableRooms,
+                  joinedRooms: this.currentUser.rooms
+              })
+          })
+          .catch(err => console.log('error on joinableRooms: ', errr))
+
+          this.currentUser.subscribeToRoom({
+              roomId: 10328672,
+             // messageLimit: 20, // set message limit per room - default 20
+              hooks: {
+                  onNewMessage: message => {
+                      this.setState({
+                          messages: [...this.state.messages, message]
+                      })
+                  }
+              }
+          })
+      })
+      .catch(err => console.log('error on connecting: ', errr))
+
+    }
+
+    snedMessage(text) {
+        this.currentUser.sendMessage({
+            text,
+            roomId: 10328672
+        })
     }
 
     render() {
         return (
             <div className="app">
-                <RoomList />
-                <MessageList />
-                <SendMessageForm />
+                <RoomList rooms={...this.state.joinableRooms. ...this.state.joinedRooms}/>
+                <MessageList messages={this.state.messages} />
+                <SendMessageForm sendMessage={this.sendMessage} />
                 <NewRoomForm />
             </div>
         );
---
+++ config.js
@@ -0,0 +1,5 @@
+const tokenUrl =
+"https://us1.pusherplatform.io/services/chatkit_token_provider/v1/06c170e7-50b3-4e78-b329-1c2434419b9c/token";
+const instanceLocator = "v1:us1:06c170e7-50b3-4e78-b329-1c2434419b9c";
+
+export { tokenUrl, instanceLocator }
